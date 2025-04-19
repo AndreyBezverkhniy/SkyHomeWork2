04995d843724f801366dd3cbf5f779f36bdb92a7
banbezv <banbezv@mail.ru>
2025-04-19 18:12:24 +0300

    HW-8

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -10,60 +10,39 @@ import org.skypro.skyshop.product.Product;
 import org.skypro.skyshop.product.SimpleProduct;
 import org.skypro.skyshop.searchEngine.SearchEngine;
 
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class App {
     public static void main(String[] args) {
-        ProductBasket productBasket = new ProductBasket();
-        productBasket.add(new SimpleProduct("hotcat1", 100));
-        productBasket.add(new SimpleProduct("hotcat2", 200));
-        productBasket.add(new SimpleProduct("hotcat1", 300));
-        productBasket.add(new SimpleProduct("hotcat2", 400));
-        productBasket.add(new SimpleProduct("hotcat1", 500));
-        productBasket.add(new SimpleProduct("hotcat2", 600));
-        productBasket.printBasket();
-        List<Product> removedProducts;
-        String[] queries = {"", "hotcat1"};
-        for (String query : queries) {
-            System.out.println("Удаляем \"" + query + "\"");
-            removedProducts = productBasket.remove(query);
-            System.out.println("Остались");
-            productBasket.printBasket();
-            if (removedProducts.isEmpty()) {
-                System.out.println("Ничего не удалено");
-                continue;
-            }
-            for (Product removedProduct : removedProducts) {
-                System.out.println("Удалён " + removedProduct);
-            }
-        }
         SearchEngine searchEngine = new SearchEngine();
         Searchable[] searchablesToAdd = {
-                new SimpleProduct("0_app0apple0", 101),
-                new SimpleProduct("2_app1apppp", 102),
-                new SimpleProduct("7_app2apppp", 103),
-                new SimpleProduct("8_app3apple1", 104),
-                new SimpleProduct("5_app4apppp", 105),
-                new SimpleProduct("3_app5apppp", 106),
-                new SimpleProduct("9_app6apppp", 107),
-                new SimpleProduct("6_app7apple2", 108),
-                new SimpleProduct("4_app8apple3", 109),
-                new SimpleProduct("1_app9apppp", 110),
+                new Article("Dapple_double", "Text"),
+                new Article("Dapple_double", "Text"),
+                new Article("length_1", "Text"),
+                new Article("length_333", "Text"),
+                new Article("length_22", "Text"),
+                new Article("length_4444", "Text"),
+                new Article("length_long_alphabet_abc", "Text"),
+                new Article("length_long_alphabet_acc", "Text"),
+                new Article("length_long_alphabet_aac", "Text"),
         };
         for (Searchable searchable : searchablesToAdd) {
             searchEngine.add(searchable);
         }
-        queries = new String[]{"none", "app", "apple"};
+        String[] queries = new String[]{"abc","long",""};
         for (String query : queries) {
             System.out.println("Search for \"" + query + "\"");
-            Map<String, Searchable> foundResults = searchEngine.search(query);
+            Set<Searchable> foundResults = searchEngine.search(query);
             if (foundResults.isEmpty()) {
                 System.out.println("Результатов нет");
                 continue;
             }
-            for (Map.Entry<String, Searchable> foundResult : foundResults.entrySet()) {
-                System.out.println("Найдено: " + foundResult.getKey() + " = " + foundResult.getValue());
+            for (Searchable foundResult : foundResults) {
+                System.out.println("Найдено: " + foundResult.getSearchableName());
             }
         }
     }
--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -2,6 +2,8 @@ package org.skypro.skyshop.product;
 
 import org.skypro.skyshop.interfaces.Searchable;
 
+import java.util.Objects;
+
 public abstract class Product implements Searchable {
     private String name;
 
@@ -34,4 +36,18 @@ public abstract class Product implements Searchable {
     public abstract int getPrice();
 
     public abstract boolean isSpecial();
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || !(o instanceof Product)) {
+            return false;
+        }
+        Product other = (Product) o;
+        return Objects.equals(name, other.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
